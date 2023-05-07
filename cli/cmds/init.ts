2b53e7adca0bad7f796db3a23277581526a79b75
Evan Conrad <evan@roomservice.dev>
2023-05-07 12:09:32 -0700

    add init

---
+++ cli/cmds/init.ts
@@ -0,0 +1,13 @@
+export async function initCmd() {
+  // If there's not already a rules directory
+  // add one
+  await Deno.mkdir("rules", { recursive: true });
+
+  // Add a markdown file called "no-bugs.md"
+  await Deno.writeTextFile("rules/no-bugs.md", "don't approve obvious bugs.");
+
+  // Print out an instruction to login and then run rules check
+  console.log(`You're ready to go! Try running:
+  rules login
+  rules check`);
+}
--- cli/main.ts
+++ cli/main.ts
@@ -2,6 +2,7 @@ import { Command } from "https://deno.land/x/cliffy@v0.25.7/command/mod.ts";
 import { loginCmd } from "./cmds/login.ts";
 import { checkCmd } from "./cmds/check.ts";
 import { readVersion } from "./version.ts";
+import { initCmd } from "./cmds/init.ts";
 
 const version = await readVersion();
 
@@ -10,6 +11,8 @@ const cmd: any = new Command()
   .version(version)
   .description("The english test framework")
   .action(() => cmd.showHelp())
+  .command("init", "Add a rules folder with a demo rule")
+  .action(() => initCmd())
   .command("check", "Check this repository against all rules.")
   .option("--host [host]", "A specific api deployment of lintrule")
   .action((options, ..._args) =>
