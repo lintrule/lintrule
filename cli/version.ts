4225ce7227f6abe27f7455ac607b6b5992f0eb2f
Evan Conrad <evan@roomservice.dev>
2023-05-07 12:32:12 -0700

    fix version

--- cli/version.ts
+++ cli/version.ts
@@ -1,13 +1,5 @@
+import { version } from "./cli.json" assert { type: "json" };
+
 export async function readVersion() {
-  // Look for the 'cli.json' file
-  const cliJsonPath = "cli/cli.json";
-  const cliJson = await Deno.readTextFile(cliJsonPath);
-
-  // Parse the JSON
-  const cli = JSON.parse(cliJson);
-  if (!cli.version) {
-    throw new Error("No version found in cli.json");
-  }
-
-  return cli.version;
+  return version;
 }
