6e70b7cc5f05979d81e7c0dcad292257bf2544cb
Evan Conrad <evan@roomservice.dev>
2023-06-13 15:08:13 -0700

    use a better init

--- cli/cmds/init.ts
+++ cli/cmds/init.ts
@@ -13,7 +13,14 @@ export async function initCmd() {
     // Add a markdown file called "no-bugs.md"
     await Deno.writeTextFile(
       join(".rules", "no-bugs.md"),
-      "don't approve obvious bugs."
+      `Make sure our code follows these best practices, UNLESS there's a comment explaining why it's okay to break the rule.
+
+1. Avoid typos.
+2. Don't have like, really obvious bugs.
+3. Don't store secrets in code.
+4. Use a logging library, not console.log, print, etc.
+5. Follow reasonable conventions of the language we're programming in. No need to be too strict.
+6. Avoid dangerous stuff, like things that could lead to template injection, SQL injection, broken access control, or really anything that would show up as a CVE somewhere.`
     );
   }
 
